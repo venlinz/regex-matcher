ec8fa889b18753504ab1bfb6fec581ae84b35bf1
venkateshht <shariharanvenkatesh@gmail.com>
2021-07-19 19:08:10 +0000

    added addFilename() in args.c for refactoring cmdline args parsing

--- args.h
+++ args.h
@@ -1,9 +1,12 @@
 #ifndef ARGS_H // Start of ARGS_H
 #define ARGS_H
 
+#include "dir.h"
+
 #define OPTIONS "f:rv."
 #define EOI EOF
 
 void usage(char *);
+filenames_t * addFilename(filenames_t *, char *);
 
 #endif // end of ARGS_H
--- main.c
+++ main.c
@@ -31,55 +31,6 @@ int main(int argc, char **argv) {
         switch (opt) {
             case 'f':
                 filenames = addFilename(filenames, optarg);
-                /* if (filenames == NULL) { */
-                /*     filenames = (filenames_t *) malloc(sizeof(filenames_t)); */
-                /*     if (filenames == NULL) { */
-                /*         perror("Error on filenames_t allocation in -f in main"); */
-                /*         return EXIT_FAILURE; */
-                /*     } */
-
-                /*     size_t count; */
-                /*     filenames->count = count = 1; */
-                /*     filenames->f_names = (char **) malloc(sizeof (char *) * count); */
-                /*     if (filenames->f_names == NULL) { */
-                /*         perror("Error on filenames_t f_names allocation"); */
-                /*         return EXIT_FAILURE; */
-                /*     } */
-
-                /*     filenames->f_names[count - 1] = (char *) malloc(strlen(optarg) + 1); */
-                /*     if (filenames->f_names[count - 1] == NULL) { */
-                /*         perror("Error on filenames_t f_names[] allocation"); */
-                /*         return EXIT_FAILURE; */
-                /*     } */
-                /*     strcpy(filenames->f_names[count - 1], optarg); */
-                /* } */
-                /* else { */
-                /*     filenames->count++; */
-                /*     size_t count = filenames->count; */
-                /*     /1* char *f_names0 = filenames->f_names[0]; *1/ */
-
-                /*     char **temp = realloc(filenames->f_names, count * sizeof(char *)); */
-                /*     if (temp == NULL) { */
-                /*         perror("Error on filenames_t f_names reallocation in main"); */
-                /*         return EXIT_FAILURE; */
-                /*     } */
-
-                /*     filenames->f_names = temp; */
-                /*     temp = NULL; */
-                /*     filenames->f_names[count - 1] = (char *) malloc(strlen(filenames->f_names[0]) + 1); */
-                /*     if (filenames->f_names[count - 1] == NULL) { */
-                /*         perror("Error on new filenames_t f_names[] allocation in main"); */
-                /*         return EXIT_FAILURE; */
-                /*     } */
-                /*     strcpy(filenames->f_names[count - 1], filenames->f_names[0]); */
-
-                /*     filenames->f_names[0] = realloc(filenames->f_names[0], strlen(optarg) + 1); */
-                /*     if (filenames->f_names[0] == NULL) { */
-                /*         perror("Error on filenames_t f_names[] reallocation in main"); */
-                /*         return EXIT_FAILURE; */
-                /*     } */
-                /*     strcpy(filenames->f_names[0], optarg); */
-                /* } */
                 break;
 
             case '.':
